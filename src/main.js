e409eb2629358cfead12cf9c5971f77cafd60cdb
unknown <syntynytkn@gmail.com>
2024-09-23 18:34:58 +0300

    2 new levels (June 2024)

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import Phaser from 'phaser'
+import Level1Scene from './scenes/Level1Scene'
+import Level2Scene from './scenes/Level2Scene'
+import EndScene from './scenes/EndScene'
+import DeathScene from './scenes/DeathScene'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
-
-const config = {
+var config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 1024,
+	height: 576,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 300 },
+			debug: false
 		}
 	},
-	scene: [HelloWorldScene]
-}
+	scene: [Level1Scene, Level2Scene, EndScene, DeathScene]
+	
+};
 
-export default new Phaser.Game(config)
+var game = new Phaser.Game(config);
